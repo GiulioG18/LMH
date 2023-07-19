96fe4e83367fd7ba3a362d8a403554f7e898b063
Giulio <Giulio.graziani.93@gmail.com>
2023-07-19 11:53:41 +0200

    added files

---
+++ LMH-Qt/lmhqt.cpp
@@ -0,0 +1,10 @@
+#include "lmhqt.h"
+
+LMHQt::LMHQt(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+LMHQt::~LMHQt()
+{}
---
+++ LMH-Qt/lmhqt.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_lmhqt.h"
+
+class LMHQt : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    LMHQt(QWidget *parent = nullptr);
+    ~LMHQt();
+
+private:
+    Ui::LMHQtClass ui;
+};
